d6c1878004f751b398a57829d161a0e74f6993c2
Tina Ho <ho.tina1704@gmail.com>
2023-05-18 16:27:19 -0700

    login page skeletal done

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -73,28 +73,29 @@ class ExplorePageState extends State<ExplorePage> {
     return _isLoading
         ? const Center(child: CircularProgressIndicator()) // Show loading indicator while fetching location
         : FlutterMap(
-      mapController: _mapController,
-      options: MapOptions(
-        center: _currentLocation,
-        zoom: 15,
-      ),
-      children: [
-        TileLayer(
-          urlTemplate:
-              'https://api.mapbox.com/styles/v1/petpilot/clhs37jer00mq01pz1cjaawig/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGV0cGlsb3QiLCJhIjoiY2xocjF1aTRtMmtrYzNlbzMzNzlvZHFoYSJ9.qMoZ-faYHI43IvUR3hWVow',
-          additionalOptions: const {
-            'accessToken':
-                'pk.eyJ1IjoicGV0cGlsb3QiLCJhIjoiY2xocjF1aTRtMmtrYzNlbzMzNzlvZHFoYSJ9.qMoZ-faYHI43IvUR3hWVow',
-            'id': 'mapbox.mapbox-streets-v8'
-          },
-        ),
-        MarkerLayer(
-          markers: [
-            Marker(point: _currentLocation, builder: (ctx) => _PulseAnimation())
-          ],
-        )
-      ]
-    );
+            mapController: _mapController,
+            options: MapOptions(
+              center: _currentLocation,
+              zoom: 15,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate:
+                    'https://api.mapbox.com/styles/v1/petpilot/clhs37jer00mq01pz1cjaawig/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGV0cGlsb3QiLCJhIjoiY2xocjF1aTRtMmtrYzNlbzMzNzlvZHFoYSJ9.qMoZ-faYHI43IvUR3hWVow',
+                additionalOptions: const {
+                  'accessToken':
+                      'pk.eyJ1IjoicGV0cGlsb3QiLCJhIjoiY2xocjF1aTRtMmtrYzNlbzMzNzlvZHFoYSJ9.qMoZ-faYHI43IvUR3hWVow',
+                  'id': 'mapbox.mapbox-streets-v8'
+                },
+              ),
+              MarkerLayer(
+                markers: [
+                  Marker(
+                      point: _currentLocation, builder: (ctx) => _PulseAnimation())
+                ],
+              )
+            ],
+          );
   }
 }
 
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,220 @@
+import 'package:flutter/material.dart';
+import 'package:petpilot/components/my_button.dart';
+import 'package:petpilot/components/my_textfield.dart';
+import 'package:petpilot/components/square_tile.dart';
+import 'package:petpilot/pages/explore_page.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  // text editing controllers
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // sign user in method
+  void signUserIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 10),
+
+              // logo
+              const Image(image: AssetImage('lib/images/petpilot_logo.png')),
+
+              const SizedBox(height: 10),
+
+              // username textfield
+              MyTextField(
+                controller: emailController,
+                hintText: 'Email',
+                obscureText: false,
+              ),
+
+              const SizedBox(height: 10),
+
+              // password textfield
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 10),
+
+              // forgot password?
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 10),
+
+              // sign in button
+              MyButton(onTap: signUserIn),
+
+              const SizedBox(height: 15),
+
+              // or continue with
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'Or continue with',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 15),
+
+              // google + apple sign in buttons
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // google button
+                  SquareTile(imagePath: 'lib/images/google.png'),
+
+                  SizedBox(width: 25),
+
+                  // apple button
+                  SquareTile(imagePath: 'lib/images/apple.png')
+                ],
+              ),
+
+              const SizedBox(height: 15),
+
+              // skip for now
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const ExplorePage()),
+                  );
+                },
+                child: const Text(
+                  'Skip for now',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+// import 'package:flutter/material.dart';
+// import '../main.dart';
+
+// class AuthenticationPage extends StatefulWidget {
+//   const AuthenticationPage({Key? key}) : super(key: key);
+
+//   @override
+//   AuthenticationPageState createState() => AuthenticationPageState();
+// }
+
+// class AuthenticationPageState extends State<AuthenticationPage> {
+//   bool _isLoggedIn = false;
+//   final TextEditingController _emailController = TextEditingController();
+//   final TextEditingController _passwordController = TextEditingController();
+
+//   void _login() {
+//     // Perform your login logic here
+//     setState(() {
+//       _isLoggedIn = true;
+//     });
+//   }
+
+//   void _logout() {
+//     // Perform your logout logic here
+//     setState(() {
+//       _isLoggedIn = false;
+//     });
+//   }
+
+//   @override
+//   void dispose() {
+//     _emailController.dispose();
+//     _passwordController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     if (_isLoggedIn) {
+//       return Main(logoutCallback: _logout);
+//     } else {
+//       return Scaffold(
+//         body: Center(
+//           child: Padding(
+//             padding: const EdgeInsets.all(20.0),
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 TextField(
+//                   controller: _emailController,
+//                   decoration: const InputDecoration(
+//                     labelText: 'Email',
+//                   ),
+//                 ),
+//                 const SizedBox(height: 16.0),
+//                 TextField(
+//                   controller: _passwordController,
+//                   decoration: const InputDecoration(
+//                     labelText: 'Password',
+//                   ),
+//                   obscureText: true,
+//                 ),
+//                 const SizedBox(height: 16.0),
+//                 ElevatedButton(
+//                   onPressed: _login,
+//                   style: ElevatedButton.styleFrom(backgroundColor: themeColor),
+//                   child: const Text('Sign-Up')
+//                 ),
+//               ],
+//             ),
+//           )
+//         )
+//       );
+//     }
+//   }
+// }
