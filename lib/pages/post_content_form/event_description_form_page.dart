233aea6db9781ab2817bc9778f7a52132a0ee1d4
Tina Ho <ho.tina1704@gmail.com>
2023-06-24 13:45:47 -0700

    finished all form pages

--- lib/pages/post_content_form/event_description_form_page.dart
+++ lib/pages/post_content_form/event_description_form_page.dart
@@ -7,14 +7,23 @@ class EventDescriptionFormPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
-        Text('EventDescription'),
-        // ElevatedButton(
-        //   onPressed: () {
-        //     // Go back to Page 3
-        //     Navigator.of(context).pop();
-        //   },
-        //   child: const Text('Go back'),
-        // ),
+        SizedBox(height: 20),
+        Text(
+          'Event Description',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 20),
+        TextField(
+          maxLines: 5, // Adjust the number of lines as per your requirement
+          maxLength: 250, // Set the character limit for the event description
+          decoration: InputDecoration(
+            hintText: 'Enter event description',
+            border: OutlineInputBorder(),
+          ),
+        ),
       ],
     );
   }
